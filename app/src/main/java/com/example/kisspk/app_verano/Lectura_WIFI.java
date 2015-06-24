19aa444649f0fb0857ed6941f0e9b30af9816160
Jordi Alejandro García Núñez <jordign.mx@hotmail.com>
2015-06-24 16:16:31 -0500

    update
    Guarda los datos como deben de ser, pero aún no se guarda con el nombre
    correcto

--- app/src/main/java/com/example/kisspk/app_verano/Lectura_WIFI.java
+++ app/src/main/java/com/example/kisspk/app_verano/Lectura_WIFI.java
@@ -14,27 +14,41 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 import java.util.List;
+
+//clases importadas para guardar datos
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import android.os.Environment;
+import android.widget.Toast;
+
+//Para el uso de itertor
+import java.util.Iterator;
 
 
 /*public class Lectura_WIFI extends ActionBarActivity*/
 public class Lectura_WIFI extends Activity implements View.OnClickListener {
     ListView lv;
     WifiManager wifi;
-    String wifis[];
+    String[] wifis;
     Button btn;
+    Button btn2;
     WifiScanReceiver wifiReciever;
-    /*Context ctx;
-    Intent inte;*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lectura__wifi);
-        lv=(ListView)findViewById(R.id.listView);
-        btn= (Button) findViewById(R.id.button);
+        lv = (ListView) findViewById(R.id.listView);
+        btn = (Button) findViewById(R.id.button);
         btn.setOnClickListener(this);
-        wifi=(WifiManager)getSystemService(Context.WIFI_SERVICE);
+        btn2 = (Button) findViewById(R.id.button2);
+        btn2.setOnClickListener(this);
+        wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
         wifiReciever = new WifiScanReceiver();
         wifi.startScan();
     }
@@ -66,23 +80,82 @@ public class Lectura_WIFI extends Activity implements View.OnClickListener {
         return super.onOptionsItemSelected(item);
     }
 
-    private class WifiScanReceiver extends BroadcastReceiver{
+    private class WifiScanReceiver extends BroadcastReceiver {
         public void onReceive(Context c, Intent intent) {
             List<ScanResult> wifiScanList = wifi.getScanResults();
             wifis = new String[wifiScanList.size()];
 
-            for(int i = 0; i < wifiScanList.size(); i++){
-                wifis[i] = ((wifiScanList.get(i).BSSID).toString()+ "," +(wifiScanList.get(i).level));
+            for (int i = 0; i < wifiScanList.size(); i++) {
+                wifis[i] = ((wifiScanList.get(i).BSSID).toString() + "," + (wifiScanList.get(i).level));
             }
-            lv.setAdapter(new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,wifis));
+            lv.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, wifis));
         }
 
     }
 
-    public void onClick(View view)
-    {
+    public void onClick(View arg0) {
         wifi.startScan();
+        // TODO Auto-generated method stub
+        File sdCard, directory, file = null;
+
+        try {
+            // validamos si se encuentra montada nuestra memoria externa
+            if (Environment.getExternalStorageState().equals("mounted")) {
+
+                // Obtenemos el directorio de la memoria externa
+                sdCard = Environment.getExternalStorageDirectory();
+
+                if (arg0.equals(btn2)) {
+
+                    // Clase que permite grabar texto en un archivo
+                    FileOutputStream fout = null;
+                    try {
+                        // instanciamos un onjeto File para crear un nuevo
+                        // directorio
+                        // la memoria externa
+                        directory = new File(sdCard.getAbsolutePath()
+                                + "/Mis archivos");
+                        // se crea el nuevo directorio donde se cerara el
+                        // archivo
+                        directory.mkdirs();
+
+                        // creamos el archivo en el nuevo directorio creado
+                        file = new File(directory, "MiArchivo.txt");
+
+                        fout = new FileOutputStream(file);
+
+                        // Convierte un stream de caracteres en un stream de
+                        // bytes
+
+                        OutputStreamWriter ows = new OutputStreamWriter(fout);
+                        //se crea un arraylist para poder hacer las iteraciones
+
+                        for (String nombre : wifis) {
+                            ows.write(nombre.toString()+"\n");
+                        }
+                        // Escribe en el buffer la cadena de texto
+                        ows.flush(); // Volca lo que hay en el buffer al archivo
+                        ows.close(); // Cierra el archivo de texto
+
+                        Toast.makeText(getBaseContext(),
+                                "El archivo se ha almacenado!!!",
+                                Toast.LENGTH_SHORT).show();
+
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+
+                }
+            }else{
+                Toast.makeText(getBaseContext(),
+                        "El almacenamineto externio no se encuentra disponible",
+                        Toast.LENGTH_LONG).show();
+            }
+
+        }catch (Exception e) {
+            // TODO: handle exception
+
+        }
     }
-
-
 }
