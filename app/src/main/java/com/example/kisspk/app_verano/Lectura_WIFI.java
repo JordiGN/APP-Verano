b707633c813c66bb5e80d12e68855a9084536fc4
Angelica Barreda B <anjelyk.b@gmail.com>
2015-06-29 11:05:11 -0500

    Update
    Versión que guarda lista con promedio

--- app/src/main/java/com/example/kisspk/app_verano/Lectura_WIFI.java
+++ app/src/main/java/com/example/kisspk/app_verano/Lectura_WIFI.java
@@ -31,12 +31,20 @@ public class Lectura_WIFI extends Activity implements View.OnClickListener {
     ListView lv;
     WifiManager wifi;
     String[] wifis;
+    String[] wifis2;
+    String[] wifistemp;
+    int contar=0;
     Button btn;//botón para volver a hacer un escaneo
     Button btn2;//botón para guardar el scanresult actual
     Button btn3;//botón para reiniciar el contador de ubicación y de instancia
     WifiScanReceiver wifiReciever;
     int cont=1;
     int ub=1;
+    int j=0;
+    int y=0;
+    //String t1,t2;
+    int tam1,tam2;
+    String[] wf2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,12 +93,84 @@ public class Lectura_WIFI extends Activity implements View.OnClickListener {
     private class WifiScanReceiver extends BroadcastReceiver {
         public void onReceive(Context c, Intent intent) {
             List<ScanResult> wifiScanList = wifi.getScanResults();
-            wifis = new String[wifiScanList.size()];
 
-            for (int i = 0; i < wifiScanList.size(); i++) {
-                wifis[i] = ((wifiScanList.get(i).BSSID).toString() + "," + (wifiScanList.get(i).level));
+            contar++;
+            //Toast.makeText(getBaseContext(), "El contar esta en "+contar +" !!", Toast.LENGTH_SHORT).show();
+            if(contar==2){
+                wifis2= new String[wifiScanList.size()];
+            }else{
+                wifis = new String[wifiScanList.size()];
             }
-            lv.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, wifis));
+
+           for (int i = 0; i < wifiScanList.size(); i++) {
+                if(contar==2){
+
+                    wifis2[i] = ((wifiScanList.get(i).BSSID).toString() + "," + (wifiScanList.get(i).level));
+                    //Toast.makeText(getBaseContext(), "en wifi2", Toast.LENGTH_SHORT).show();
+
+
+                }else{
+
+                    wifis[i] = ((wifiScanList.get(i).BSSID).toString() + "," + (wifiScanList.get(i).level));
+                    //Toast.makeText(getBaseContext(), "en wifi1", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+            if(contar==2){
+                //Mostrar la lista
+                lv.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, wifis2));
+                tam2=wifis2.length;
+                if(tam1>tam2){
+                    wifistemp= new String[tam2];
+                    for(int b=0;b<tam2;b++){
+                        wifistemp[b]=wifis2[b];
+                    }
+                    //String t1=wifis2[0];
+                    //String t2=wifis[0];
+                    wf2= new String[tam1];
+                    for(int n=0;n<tam1;n++){
+                        wf2[n]=wifis[n];
+                    }
+
+                }else{
+                    wifistemp= new String[tam1];
+                    for(int a=0;a<tam1;a++){
+                        wifistemp[a]=wifis[a];
+                    }
+                    //String t1=wifis[0];
+                    //String t2=wifis2[0];
+                    wf2= new String[tam2];
+                    for(int n=0;n<tam2;n++){
+                        wf2[n]=wifis2[n];
+                    }
+                }
+                while(j<(wifistemp.length)){
+                    String t1=wifistemp[j];
+                    String [] ttemp=t1.split(",");
+                    String mac1=ttemp[0];
+                    int inten1=Integer.parseInt(ttemp[1]);
+
+                    for(int x=0;x<(wf2.length);x++){
+                        String t2=wf2[x];
+                        String[] ttemp2=t2.split(",");
+                        String mac2=ttemp2[0];
+                        int inte2=Integer.parseInt(ttemp2[1]);
+                        if(mac1.equals(mac2)){
+                            int intetemp=((inten1+inte2)/2);
+                            String it=Integer.toString(intetemp);
+                            wifistemp[j]=mac1+it;
+                            x=wf2.length;
+                        }
+                    }
+                    j++;
+                }
+
+            }else {
+                //Mostrar la lista
+                lv.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, wifis));
+                tam1=wifis.length;
+            }
+
         }
 
     }
@@ -98,7 +178,9 @@ public class Lectura_WIFI extends Activity implements View.OnClickListener {
     public void onClick(View arg0) {
 
         if (arg0.equals(btn))
-        {wifi.startScan();}
+        {
+            wifi.startScan();
+        }
 
         // TODO Auto-generated method stub
         File sdCard, directory, file = null;
@@ -108,53 +190,62 @@ public class Lectura_WIFI extends Activity implements View.OnClickListener {
             ub=1;
         }
 
-        try {
+        try
+        {
             // validamos si se encuentra montada nuestra memoria externa
-            if (Environment.getExternalStorageState().equals("mounted")) {
+            if (Environment.getExternalStorageState().equals("mounted"))
+            {
                 // Obtenemos el directorio de la memoria externa
                 sdCard = Environment.getExternalStorageDirectory();
-                if (arg0.equals(btn2)) {
-                    // Clase que permite grabar texto en un archivo
-                    FileOutputStream fout = null;
-                    try {
-                        // instanciamos un objeto File para crear un nuevo
-                        // directorio
-                        // la memoria externa
-                        directory = new File(sdCard.getAbsolutePath()
-                                + "/Instancias");
-                        // se crea el nuevo directorio donde se cerara el
-                        // archivo
-                        directory.mkdirs();
+                if (arg0.equals(btn2))
+                {
+                    if(contar==2)
+                    {
+                        // Clase que permite grabar texto en un archivo
+                        FileOutputStream fout = null;
+                        try {
+                            // instanciamos un objeto File para crear un nuevo
+                            // directorio
+                            // la memoria externa
+                            directory = new File(sdCard.getAbsolutePath()
+                                    + "/Instancias");
+                            // se crea el nuevo directorio donde se cerara el
+                            // archivo
+                            directory.mkdirs();
 
-                        // creamos el archivo en el nuevo directorio creado
-                        file = new File(directory, "Ubicación"+(ub)+"_"+(cont++)+".txt");
-                        fout = new FileOutputStream(file);
-                        // Convierte un stream de caracteres en un stream de
-                        // bytes
-                       OutputStreamWriter ows = new OutputStreamWriter(fout);
-                        //se crea un arraylist para poder hacer las iteraciones
+                            // creamos el archivo en el nuevo directorio creado
+                            file = new File(directory, "Ubicación" + (ub) + "_" + (cont++) + ".txt");
+                            fout = new FileOutputStream(file);
+                            // Convierte un stream de caracteres en un stream de
+                            // bytes
+                            OutputStreamWriter ows = new OutputStreamWriter(fout);
+                            //se crea un arraylist para poder hacer las iteraciones
 
-                        for (String nombre : wifis) {
-                            ows.write(nombre.toString()+"\n");
+                            for (String nombre : wifistemp) {
+                                ows.write(nombre.toString() + "\n");
+                            }
+                            // Escribe en el buffer la cadena de texto
+                            ows.flush(); // Volca lo que hay en el buffer al archivo
+                            ows.close(); // Cierra el archivo de texto
+
+
+                            Toast.makeText(getBaseContext(),
+                                    "La instancia " + (cont - 1) + " de la ubicación " + ub + " se ha almacenado!!!",
+                                    Toast.LENGTH_SHORT).show();
+                            if (cont == 4) {
+                                cont = 1;
+                                ub++;
+                            }
+
+                        } catch (IOException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
                         }
-                        // Escribe en el buffer la cadena de texto
-                        ows.flush(); // Volca lo que hay en el buffer al archivo
-                        ows.close(); // Cierra el archivo de texto
-
-
+                    }else{
                         Toast.makeText(getBaseContext(),
-                                "La instancia "+(cont-1)+" de la ubicación "+ub+" se ha almacenado!!!",
-                                Toast.LENGTH_SHORT).show();
-                        if (cont==4){
-                            cont=1;
-                            ub++;
-                        }
-
-                    } catch (IOException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
+                                "Refresca una vez mas",
+                                Toast.LENGTH_LONG).show();
                     }
-
                 }
             }else{
                 Toast.makeText(getBaseContext(),
@@ -162,7 +253,8 @@ public class Lectura_WIFI extends Activity implements View.OnClickListener {
                         Toast.LENGTH_LONG).show();
             }
 
-        }catch (Exception e) {
+        }catch (Exception e)
+        {
             // TODO: handle exception
 
         }
